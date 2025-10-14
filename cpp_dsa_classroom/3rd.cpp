7b5e8d4e8d1d077bed9248b3333959350ed8f7de
Vivek Rahar <vivekrahar2@gmail.com>
2025-10-15 00:57:40 +0530

    Create 3rd.cpp

---
+++ cpp_dsa_classroom/3rd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x =10;
+    if (x>0)
+    {
+        cout << "Positive Number" << endl;
+    }
+    else
+    {
+        cout << "Non-Positive Number" << endl;
+    }
+}
